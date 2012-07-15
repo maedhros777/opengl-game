2abe1bad4388cfb09bfc3024b30a473da83ee6e1
maedhros777 <maedhros777@gmail.com>
2012-07-14 22:07:29 -0400

    build files

---
+++ build/CMakeFiles/3dlib.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/michael/C++/3DLib/main.cpp" "/home/michael/C++/3DLib/build/CMakeFiles/3dlib.dir/__/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/michael/C++/3DLib/source/CMakeFiles/source.dir/DependInfo.cmake"
+  )
---
+++ build/CMakeFiles/3dlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/3dlib.dir/__/main.cpp.o"
+  "3dlib.pdb"
+  "3dlib"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/3dlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
